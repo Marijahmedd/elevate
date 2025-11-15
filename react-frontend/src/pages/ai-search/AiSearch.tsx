14999031bd3c7ca02e83c7133786d38ff96e6b83
baysick <muhammadmarijahmed@gmail.com>
2025-11-16 04:41:39 +0500

    Fix: AI search frontend

--- react-frontend/src/pages/ai-search/AiSearch.tsx
+++ react-frontend/src/pages/ai-search/AiSearch.tsx
@@ -1,17 +1,23 @@
-import { BackgroundLines } from "@/components/ui/background-lines";
-
+import { motion } from "motion/react";
+import { LampContainer } from "../../components/ui/lamp";
+ 
 const AiSearch = () => {
     return (
-        <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-            <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-                Elevate your job hunt <br /> with AI Search.
-
-            </h2>
-            <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
-                Coming Soon...
-            </p>
-        </BackgroundLines>
-    );
+        <LampContainer>
+          <motion.h1
+            initial={{ opacity: 0.5, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+          >
+            Elevate your job hunt with AI search <br /> <div>Coming Soon!</div>    
+          </motion.h1>
+        </LampContainer>
+      );
 }
 
 export default AiSearch
